529677dcdb9601429127a07955044ef34afb8f43
John Machado <iamjohnmachado@gmail.com>
2024-11-12 11:18:44 -0600

    Coding the GPT Model

---
+++ 04_Implementing_a_GPT_model_to_generate_text/4.6_Coding_the_GPT_Model/main.py
@@ -0,0 +1,305 @@
+import torch
+import torch.nn as nn
+import tiktoken
+
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
+    
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+
+        pos_embeds = self.pos_emb(
+            torch.arange(seq_len, device=in_idx.device)
+        )
+        x = tok_embeds + pos_embeds # Shape [batch_size, num_tokens, emb_size]
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+
+        logits = self.out_head(x)
+        return logits
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"]
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+    
+    def forward(self, x):
+        # Shortcut connection for attention block
+        shortcut = x 
+        x = self.norm1(x)
+        x = self.att(x) # Shape [batch_size, num_tokens, emb_size]
+        x = self.drop_shortcut(x)
+        x = x + shortcut # Add the original input back
+
+        # Shortcut connection for feed-forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut # Add the original input back
+
+        return x
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads # Reduce the projection dim to match desired output dim
+        
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out) # Linear layer to combine head outputs
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        
+        # Shape: (b, num_tokens, d_out)
+        queries = self.W_query(x) 
+        keys = self.W_key(x)
+        values = self.W_value(x)
+
+        # We implicitly split the matrix by adding a `num_heads` dimension
+        # Unroll last dim: (b, num_tokens, d_out) -> (b, num_tokens, num_heads, head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # Transpose: (b, num_tokens, num_heads, head_dim) -> (b, num_heads, num_tokens, head_dim)
+        queries = queries.transpose(1, 2)
+        keys = keys.transpose(1, 2)
+        values = values.transpose(1, 2)
+        
+        # Compute scaled dot-product attention (aka self-attention) with a causal mask
+        attn_scores = queries @ keys.transpose(2, 3)  # Dot product for each head
+
+        # Original mask truncated to the number of tokens and converted to boolean
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+        
+        # Use the mask to fill attention scores
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # Shape: (b, num_tokens, num_heads, head_dim)
+        context_vec = (attn_weights @ values).transpose(1, 2)
+
+        # Combine heads, where self.d_out = self.num_heads * self.head_dim
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec) # optional projection
+
+        return context_vec
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5 # small constant "epsilon" added to the variance, prevents division by zero during normalization
+        self.scale = nn.Parameter(torch.ones(emb_dim)) # trainable param, LLM will adjust this during training
+        self.shift = nn.Parameter(torch.zeros(emb_dim)) # trainable param, LLM will adjust this during training
+    
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"])
+        )
+    
+    def forward(self, x):
+        return self.layers(x)
+
+
+# know as "GPT-2 small"
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,        # Vocabulary size
+    "context_length": 1024,     # Context length
+    "emb_dim": 768,             # Embedding dimension
+    "n_heads": 12,              # Number of attention heads
+    "n_layers": 12,             # Number of layers
+    "drop_rate": 0.1,           # Dropout rate
+    "qkv_bias": False           # Query-Key-Value bias
+}
+
+
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+
+batch = torch.stack(batch, dim=0)
+# print(batch)
+"""
+tensor([[6109, 3626, 6100,  345], <--- token IDs of txt1
+        [6109, 1110, 6622,  257]]) <--- token IDs of txt2
+"""
+
+torch.manual_seed(123)
+
+model = GPTModel(GPT_CONFIG_124M)
+# print(model)
+
+out = model(batch)
+print("\nInput batch:\n", batch)
+print("Output batch:\n", out.shape)
+print(out)
+
+total_params = sum(p.numel() for p in model.parameters()) # numel = "number of elements"
+
+# 163,009,536, due to "weight tying", used in original GPT-2 architecture
+# Means the GPT-2 architecture resuses the weights from the token embedding layer in its output layer.
+print(f"\nTotal number of parameters: {total_params:,}")
+
+print("\nToken embedding layer shape:", model.tok_emb.weight.shape)
+print("Output layer shape:", model.out_head.weight.shape)
+
+# Removing output layer parameter count from the total GPT-2 model count
+total_params_gpt2 = (
+    total_params - sum(p.numel() for p in model.out_head.parameters())
+)
+
+print(f"\nNumber of trainable parameters considering weight tying: {total_params_gpt2:,}\n")
+
+# Computing memory requirements of the 163 million parameters
+total_size_bytes = total_params * 4 # calculates the total size in bytes (assuming float32), 4 bytes per parameter
+total_size_mb = total_size_bytes / (1024 * 1024) # converts to megabytes
+print(f"\nTotal size of the model: {total_size_mb:.2f} MB\n")
+
+
+print("-----------------------------------------------------------------")
+# Exercise 4.1 Number of parameters in feed forward and attention modules
+block = TransformerBlock(GPT_CONFIG_124M)
+
+ffn_params = sum(p.numel() for p in block.ff.parameters())
+print(f"1 Feed forward module parameters: {ffn_params:,}")
+
+multi_att_params = sum(p.numel() for p in block.att.parameters())
+print(f"1 Multi attention module parameters: {multi_att_params:,}")
+
+ffn_params_total = sum(p.numel() for p in block.ff.parameters()) * 12
+print(f"\n12 Feed forward module parameters: {ffn_params_total:,}")
+
+multi_att_params_total = sum(p.numel() for p in block.att.parameters()) * 12
+print(f"12 Multi attention module parameters: {multi_att_params_total:,}")
+
+print("\n-----------------------------------------------------------------")
+# Exercise 4.2 Initializing larger GPT models
+
+GPT_CONFIG = {
+    "vocab_size": 50257,        # Vocabulary size
+    "context_length": 1024,     # Context length
+    "emb_dim": 768,             # Embedding dimension
+    "n_heads": 12,              # Number of attention heads
+    "n_layers": 12,             # Number of layers
+    "drop_rate": 0.1,           # Dropout rate
+    "qkv_bias": False           # Query-Key-Value bias
+}
+
+def get_config(base_config, model_name="gpt2-small"):
+    GPT_CONFIG = base_config.copy()
+
+    if model_name == "gpt2-small":
+        GPT_CONFIG["emb_dim"] = 768
+        GPT_CONFIG["n_layers"] = 12
+        GPT_CONFIG["n_heads"] = 12
+    
+    elif model_name == "gpt2-medium":
+        GPT_CONFIG["emb_dim"] = 1024
+        GPT_CONFIG["n_layers"] = 24
+        GPT_CONFIG["n_heads"] = 16
+    
+    elif model_name == "gpt2-large":
+        GPT_CONFIG["emb_dim"] = 1280
+        GPT_CONFIG["n_layers"] = 36
+        GPT_CONFIG["n_heads"] = 20
+    
+    elif model_name == "gpt2-xl":
+        GPT_CONFIG["emb_dim"] = 1600
+        GPT_CONFIG["n_layers"] = 48
+        GPT_CONFIG["n_heads"] = 25
+    
+    else:
+        raise ValueError(f"Incorrect model name {model_name}")
+    
+    return GPT_CONFIG
+
+
+def calculate_size(model):
+
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"\nTotal number of parameters: {total_params:,}")
+
+    total_params_gpt2 = total_params - sum(p.numel() for p in model.out_head.parameters())
+    print(f"Number of trainable parameters considering weight tying: {total_params_gpt2:,}")
+    
+    # Calculate the total size in bytes (assuming float32, 4 bytes per parameter)
+    total_size_bytes = total_params * 4
+    
+    # Convert to megabytes
+    total_size_mb = total_size_bytes / (1024 * 1024)
+    
+    print(f"Total size of the model: {total_size_mb:.2f} MB")
+
+
+for model_abbrev in ("small", "medium", "large", "xl"):
+    model_name = f"gpt2-{model_abbrev}"
+    CONFIG = get_config(GPT_CONFIG, model_name=model_name)
+    model = GPTModel(CONFIG)
+    print(f"\n\n{model_name}:")
+    calculate_size(model)
