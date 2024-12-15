1e384ff8bc1ff8908d26484c80b6968ad8b448f6
John Machado <iamjohnmachado@gmail.com>
2024-12-15 12:06:15 -0600

    Typo fix

--- 06_Fine_tuning_for_classification/6.8_Using_the_LLM_as_a_spam_classifier/inference_playground.py
+++ 06_Fine_tuning_for_classification/6.8_Using_the_LLM_as_a_spam_classifier/inference_playground.py
@@ -623,7 +623,7 @@ spam = {
     # that blend conversational language and spammy content could help the model recognize these patterns better.
     "text_9": "Hi friend, see how I made $2,000 daily without leaving my couch. Limited-time opportunity!",
 
-    # Small change to this ^, adding "Click this link: http://claim-your-prize-free.com/winner." and the model classifies this now as "spam".
+    # Small change to this ^, adding "Click this link: http://claim-your-prize-free.com/winner" and the model classifies this now as "spam".
     "text_10": "Hi friend, see how I made $2,000 daily without leaving my couch. Limited-time opportunity! Click this link: http://claim-your-prize-free.com/winner",
     "text_11": "You qualify for a free iPhone! Click and enter your shipping details to claim yours.",
     "text_12": "Don't miss out—only a few hours left to claim your lottery prize worth $10,000. Hurry!"
